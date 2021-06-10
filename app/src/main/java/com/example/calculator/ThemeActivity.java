2b586d178aa077318afc3cdfaaaa8c3261580662
vadim <v4dikos@yandex.ru>
2021-06-10 18:05:41 +0700

    Добавлены кнопки для смены темы

--- app/src/main/java/com/example/calculator/ThemeActivity.java
+++ app/src/main/java/com/example/calculator/ThemeActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ThemeActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ThemeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        findViewById(R.id.light_button).setOnClickListener(v -> {
+
+        });
+
+        findViewById(R.id.dark_button).setOnClickListener(v -> {
+
+        });
     }
 }
